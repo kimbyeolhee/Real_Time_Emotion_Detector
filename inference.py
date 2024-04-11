7f2ad533eeabed4fad042f571e80bea65627b816
KimByeolhee <qufgml0216@naver.com>
2024-04-12 02:20:26 +0900

    [Refactor]  include face detection and emotion classification

--- inference.py
+++ inference.py
@@ -1,39 +1,63 @@
+import cv2
+from PIL import Image
+import numpy as np
+import argparse
+from omegaconf import OmegaConf
+
 from transformers import ViTImageProcessor, ViTForImageClassification
 from PIL import Image
-import cv2
 
 
-model_name = "google/vit-base-patch16-224"
-processor = ViTImageProcessor.from_pretrained(model_name)
-model = ViTForImageClassification.from_pretrained(model_name)
+def main(config):
+    face_recognizer = cv2.CascadeClassifier(config.model.face_detector.name_or_path)
 
-cap = cv2.VideoCapture(0)
+    processor = ViTImageProcessor.from_pretrained(config.model.emotion_classifier.name_or_path)
+    model = ViTForImageClassification.from_pretrained(config.model.emotion_classifier.name_or_path)
 
-while True:
-    ret, frame = cap.read()
+    cap = cv2.VideoCapture(0)
 
-    if not ret:
-        print("Error: failed to capture image")
-        break
+    while True:
+        ret, frame = cap.read() # Capture frame-by-frame
+        
+        if not ret:
+            print("Error: failed to capture image")
+            break
 
-    image = Image.fromarray(frame)
-    inputs = processor(images=image, return_tensors="pt")
+        face = face_recognizer.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
 
-    outputs = model(**inputs)
-    logits = outputs.logits
+        if len(face) > 0:
+            face = sorted(face, reverse=True, key=lambda x: (x[2] - x[0]) * (x[3] - x[1]))[0]
+            (fX, fY, fW, fH) = face
+            roi = frame[fY:fY + fH, fX:fX + fW]
+            roi = cv2.resize(roi, (config.crop_size, config.crop_size))
+            roi = cv2.cvtColor(roi, cv2.COLOR_BGR2RGB)
+            image = Image.fromarray(roi)
+            inputs = processor(images=image, return_tensors="pt")
 
-    predicted_class_idx = logits.argmax(-1).item() # imagenet 1000 class로 학습해놓은 거라 1000개의 class 중 하나가 나옴
+            outputs = model(**inputs)
+            logits = outputs.logits
 
-    prediction = model.config.id2label[predicted_class_idx]
-    print(f"Predicted class: {prediction}")
+            predicted_class_idx = logits.argmax(-1).item()
 
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(img=frame, text=prediction, org=(50, 50), fontFace=font, fontScale=1, color=(0, 255, 255), thickness=2, lineType=cv2.LINE_4)
+            prediction = model.config.id2label[predicted_class_idx]
+            print(f"Predicted class: {prediction}")
 
-    cv2.imshow("video", frame)
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.putText(img=frame, text=prediction, org=(50, 50), fontFace=font, fontScale=1, color=(0, 255, 255), thickness=2, lineType=cv2.LINE_4)
 
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
+            cv2.imshow("video", frame)
 
-cap.release()
-cv2.destroyAllWindows()
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--config", "-c", type=str, default="inference_config", help="config file path")
+    args, _ = parser.parse_known_args()
+    config = OmegaConf.load(f"./configs/{args.config}.yaml")
+
+    main(config)
